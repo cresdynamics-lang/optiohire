63d386f478991882dc08ce2ee3cd8d606f24884b
brian.oyamo <brian.oyamo@csm.tech>
2026-06-10 16:45:47 +0300

    enhancements: add account deletion

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { getCurrentUser, updateUserCompany } from '../api/userController.js'
+import { getCurrentUser, updateUserCompany, deleteSelf } from '../api/userController.js'
 import { authenticate, optionalAuthenticate } from '../middleware/auth.js'
 
 export const router = Router()
 
 router.get('/me', optionalAuthenticate, getCurrentUser)
 router.put('/company', authenticate, updateUserCompany)
+router.delete('/delete-account', authenticate, deleteSelf)
 
